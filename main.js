53e0ff997a05b7e9d263dc0de2a4f107146186e8
sankalp92011 <120492496+sankalp92011@users.noreply.github.com>
2023-01-19 17:51:45 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,19 +3,19 @@ function setup(){
     video.size(550,500);
     canvas = createCanvas(550,550);
     canvas.position(560,150);
-    poseNet = ml5.poseNet(video,modelLoaded);
+    poseNet = ml5.poseNet(video, modelLoaded);
     poseNet.on('pose',gotPoses);
 }
-function modelLoaded(){
-    console.log('PoseNet Is Initialized !!');
-}
 function draw(){
-    background('#ff0000');
+    background('#800000');
+}
+function modelLoaded(){
+    console.log('PoseNet is loaded !!');
 }
 function gotPoses(result)
 {
     if(result.length > 0)
     {
-        console.log(result);
+        console.log(results);
     }
 }
